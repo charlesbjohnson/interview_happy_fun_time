aa694710f44b6d1af8be273e822e14113566b3f5
Charles B Johnson <mail@cbjohnson.info>
2013-10-25 21:00:56 -0400

    Implement a simple binary search tree map for use in chapter 4

---
+++ lib/data_structures/binary_search_tree_map.rb
@@ -0,0 +1,155 @@
+module DataStructures
+  class BinarySearchTreeMap
+    include Enumerable
+
+    def initialize
+      @root = nil
+    end
+
+    def size
+      r_size(@root)
+    end
+
+    def each(order = nil, &block)
+      items = []
+
+      case order
+      when :pre
+        r_each_preorder(@root, items)
+      when :post
+        r_each_postorder(@root, items)
+      else
+        r_each_inorder(@root, items)
+      end
+
+      enum = items.to_enum
+      block_given? ? enum.each(&block) : enum
+    end
+
+    def get(key)
+      cursor, result = @root, nil
+      until cursor.nil?
+        if key < cursor.key
+          cursor = cursor.left
+        elsif key > cursor.key
+          cursor = cursor.right
+        else
+          result = cursor.value
+          break
+        end
+      end
+
+      result
+    end
+
+    def min
+      return nil if @root.nil?
+      min = r_min(@root)
+      [min.key, min.value]
+    end
+
+    def put(key, value)
+      @root = r_put(@root, key, value)
+    end
+
+    def delete_min
+      @root = r_delete_min(@root)
+    end
+
+    def delete(key)
+      @root = r_delete(@root, key)
+    end
+
+    private
+
+    def r_size(cursor)
+      return 0 if cursor.nil?
+      cursor.count
+    end
+
+    def r_each_inorder(cursor, items)
+      return if cursor.nil?
+
+      r_each_inorder(cursor.left, items)
+      items.push([cursor.key, cursor.value])
+      r_each_inorder(cursor.right, items)
+    end
+
+    def r_each_preorder(cursor, items)
+      return if cursor.nil?
+
+      items.push([cursor.key, cursor.value])
+      r_each_preorder(cursor.left, items)
+      r_each_preorder(cursor.right, items)
+    end
+
+    def r_each_postorder(cursor, items)
+      return if cursor.nil?
+
+      r_each_postorder(cursor.left, items)
+      r_each_postorder(cursor.right, items)
+      items.push([cursor.key, cursor.value])
+    end
+
+    def r_put(cursor, key, value)
+      return TreeNode.new(key, value, c: 1) if cursor.nil?
+
+      if key < cursor.key
+        cursor.left = r_put(cursor.left, key, value)
+      elsif key > cursor.key
+        cursor.right = r_put(cursor.right, key, value)
+      else
+        cursor.value = value
+      end
+
+      cursor.count = 1 + r_size(cursor.left) + r_size(cursor.right)
+      cursor
+    end
+
+    def r_min(cursor)
+      return cursor if cursor.left.nil?
+      r_min(cursor.left)
+    end
+
+    def r_delete_min(cursor)
+      return if cursor.nil?
+      return cursor.right if cursor.left.nil?
+
+      cursor.left = r_delete_min(cursor.left)
+
+      cursor.count = 1 + r_size(cursor.left) + r_size(cursor.right)
+      cursor
+    end
+
+    def r_delete(cursor, key)
+      return nil if cursor.nil?
+
+      if key < cursor.key
+        cursor.left = r_delete(cursor.left, key)
+      elsif key > cursor.key
+        cursor.right = r_delete(cursor.right, key)
+      else
+        return cursor.left if cursor.right.nil?
+
+        new = cursor
+        cursor = r_min(new.right)
+        cursor.right = r_delete_min(new.right)
+        cursor.left = new.left
+      end
+
+      cursor.count = 1 + r_size(cursor.left) + r_size(cursor.right)
+      cursor
+    end
+
+  end
+
+  class TreeNode
+    attr_accessor :key, :value, :left, :right, :count
+
+    def initialize(k = nil, v = nil, c: nil)
+      @key, @value = k, v
+      @count = c
+    end
+
+  end
+end
---
+++ test/data_structures/test_binary_search_tree_map.rb
@@ -0,0 +1,258 @@
+require 'config_test'
+
+describe DataStructures::BinarySearchTreeMap do
+
+  subject { DataStructures::BinarySearchTreeMap.new }
+
+  it { subject.must_respond_to :size }
+  it { subject.must_respond_to :each }
+  it { subject.must_respond_to :get }
+  it { subject.must_respond_to :min }
+  it { subject.must_respond_to :put }
+  it { subject.must_respond_to :delete_min }
+  it { subject.must_respond_to :delete }
+
+  let(:key) { :key }
+  let(:val) { 'val' }
+
+  it 'starts out empty' do
+    subject.size.must_equal 0
+  end
+
+  describe '#get' do
+    it 'returns nil for nonexistent key' do
+      subject.get(key).must_be_nil
+    end
+  end
+
+  describe '#min' do
+    it 'returns the key value pair with the smallest key' do
+      a, b = :a, :b
+      subject.put(b, b.to_s)
+      subject.put(a, a.to_s)
+      subject.put(key, val)
+
+      subject.min.must_equal([a, a.to_s])
+    end
+
+    describe 'empty' do
+      it 'returns nil' do
+        subject.min.must_be_nil
+      end
+    end
+  end
+
+  describe '#put' do
+    it 'sets the value for a key' do
+      subject.put(key, val)
+
+      subject.get(key).must_equal val
+      subject.size.must_equal 1
+    end
+
+    describe 'existing key' do
+      it 'changes the value for the key' do
+        subject.put(key, 'foo')
+        subject.put(key, val)
+
+        subject.get(key).must_equal val
+        subject.size.must_equal 1
+      end
+    end
+
+    describe 'multiple' do
+      it 'sets the value for each key' do
+        a, b = :a, :b
+        subject.put(b, b.to_s)
+        subject.put(a, a.to_s)
+        subject.put(key, val)
+
+        subject.get(a).must_equal a.to_s
+        subject.get(b).must_equal b.to_s
+        subject.get(key).must_equal val
+        subject.size.must_equal 3
+      end
+    end
+  end
+
+  describe '#delete_min' do
+    it 'deletes the item with the smallest key' do
+      a, b = :a, :b
+      subject.put(b, b.to_s)
+      subject.put(a, a.to_s)
+      subject.put(key, val)
+
+      subject.delete_min
+
+      subject.get(b).must_equal b.to_s
+      subject.get(key).must_equal val
+      subject.get(a).must_be_nil
+      subject.size.must_equal 2
+    end
+
+    describe 'multiple' do
+      it 'deletes the items with the smallest keys' do
+        a, b = :a, :b
+        subject.put(b, b.to_s)
+        subject.put(a, a.to_s)
+        subject.put(key, val)
+
+        subject.delete_min
+        subject.delete_min
+
+        subject.get(key).must_equal val
+        subject.get(b).must_be_nil
+        subject.get(a).must_be_nil
+        subject.size.must_equal 1
+      end
+    end
+
+    describe 'empty' do
+      it 'does nothing' do
+        subject.delete_min
+        subject.size.must_equal 0
+      end
+    end
+
+    describe 'single' do
+      it 'deletes the only item' do
+        subject.put(key, val)
+
+        subject.delete_min
+
+        subject.get(key).must_be_nil
+        subject.size.must_equal 0
+      end
+    end
+
+    describe 'unbalanced' do
+      it 'deletes the item with the smallest key' do
+        a, b = :a, :b
+        subject.put(a, a.to_s)
+        subject.put(b, b.to_s)
+        subject.put(key, val)
+
+        subject.delete_min
+
+        subject.get(b).must_equal b.to_s
+        subject.get(key).must_equal val
+        subject.get(a).must_be_nil
+        subject.size.must_equal 2
+      end
+    end
+  end
+
+  describe '#delete' do
+    it 'deletes the item with the matching key' do
+      a, b = :a, :b
+      subject.put(b, b.to_s)
+      subject.put(a, a.to_s)
+      subject.put(key, val)
+
+      subject.delete(a)
+
+      subject.get(b).must_equal b.to_s
+      subject.get(key).must_equal val
+      subject.get(a).must_be_nil
+      subject.size.must_equal 2
+    end
+
+    describe 'multiple' do
+      it 'deletes the items with the matching keys' do
+        a, b = :a, :b
+        subject.put(b, b.to_s)
+        subject.put(a, a.to_s)
+        subject.put(key, val)
+
+        subject.delete(b)
+        subject.delete(a)
+
+        subject.get(key).must_equal val
+        subject.get(b).must_be_nil
+        subject.get(a).must_be_nil
+        subject.size.must_equal 1
+      end
+    end
+
+    describe 'with nonexistent key' do
+      it 'does nothing' do
+        subject.delete(:a)
+        subject.size.must_equal 0
+      end
+    end
+
+    describe 'empty' do
+      it 'does nothing' do
+        subject.put(key, val)
+
+        subject.delete(:a)
+
+        subject.get(key).must_equal val
+        subject.size.must_equal 1
+      end
+    end
+
+    describe 'single' do
+      it 'deletes the only item with the matching key' do
+        subject.put(key, val)
+
+        subject.delete(key)
+
+        subject.get(key).must_be_nil
+        subject.size.must_equal 0
+      end
+    end
+
+    describe 'unbalanced' do
+      it 'deletes the item with the matching key' do
+        a, b = :a, :b
+        subject.put(a, a.to_s)
+        subject.put(b, b.to_s)
+        subject.put(key, val)
+
+        subject.delete(a)
+
+        subject.get(b).must_equal b.to_s
+        subject.get(key).must_equal val
+        subject.get(a).must_be_nil
+        subject.size.must_equal 2
+      end
+    end
+  end
+
+  describe '#each' do
+    it 'yields each key value pair inorder by default' do
+      expected = [[:a, 1], [:b, 2], [:c, 3]].shuffle
+      expected.each { |key, value| subject.put(key, value) }
+
+      subject.each.must_be_kind_of(Enumerator)
+
+      actual = []
+      subject.each { |pair| actual << pair }
+      actual.must_equal expected.sort
+    end
+
+    it 'yields each key value pair preorder' do
+      expected = [[:b, 2], [:a, 1], [:c, 3]]
+      expected.each { |key, value| subject.put(key, value) }
+
+      subject.each.must_be_kind_of(Enumerator)
+
+      actual = []
+      subject.each(:pre) { |pair| actual << pair }
+      actual.must_equal expected
+    end
+
+    it 'yields each key value pair postorder' do
+      add = [[:b, 2], [:a, 1], [:c, 3]]
+      add.each { |key, value| subject.put(key, value) }
+
+      subject.each.must_be_kind_of(Enumerator)
+
+      actual = []
+      subject.each(:post) { |pair| actual << pair }
+      actual.must_equal [[:a, 1], [:c, 3], [:b, 2]]
+    end
+  end
+
+end
