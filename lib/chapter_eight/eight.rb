daeebc7524c419044685e86a703a72206197ec4d
Charles B Johnson <mail@cbjohnson.info>
2013-11-15 15:11:14 -0500

    Quick refactor to othello problem.

--- lib/chapter_eight/eight.rb
+++ lib/chapter_eight/eight.rb
@@ -51,12 +51,13 @@ module ChapterEight
       end
 
       def available_move?(c, r, color)
-        should_stop = proc { |token| token && token.color == color }
-
-        %i[scan_horizontal scan_vertical
-           scan_diagonal_negative_slope
-           scan_diagonal_positive_slope].each do |m|
-          return true if self.send(m, c, r, should_stop).any?
+        # horizontal, vertical, both diagonals
+        [[-1, 0, 1, 0],
+         [0, -1, 0, 1],
+         [-1, -1, 1, 1],
+         [-1, 1, 1, -1]].each do |c_dec, r_dec, c_inc, r_inc|
+          return true if scan_direction(c, r, c_dec, r_dec, c_inc, r_inc,
+                                        color).any?
         end
 
         false
@@ -68,158 +69,53 @@ module ChapterEight
       end
 
       def flip_tokens(c, r, color)
-        should_stop = proc { |token| token && token.color == color }
-        flip_h = scan_horizontal(c, r, should_stop)
-        flip_v = scan_vertical(c, r, should_stop)
-        flip_diagonal_n = scan_diagonal_negative_slope(c, r, should_stop)
-        flip_diagonal_p = scan_diagonal_positive_slope(c, r, should_stop)
-        to_flip = flip_h + flip_v + flip_diagonal_n + flip_diagonal_p
+        # horizontal, vertical, both diagonals
+        to_flip = [[-1, 0, 1, 0],
+                   [0, -1, 0, 1],
+                   [-1, -1, 1, 1],
+                   [-1, 1, 1, -1]].flat_map do |c_dec, r_dec, c_inc, r_inc|
+          scan_direction(c, r, c_dec, r_dec, c_inc, r_inc, color)
+        end
 
         to_flip.each { |col, row| token_at(col, row).flip! }
       end
 
-      def scan_horizontal(c, r, should_stop)
-        result, flip_so_far_left, flip_so_far_right = [], [], []
-        left_i, right_i = -1, 1
+      def scan_direction(c, r, c_dec, r_dec, c_inc, r_inc, stop_color)
+        result, flip_so_far_dec, flip_so_far_inc = [], [], []
 
-        left, right = (c + left_i), (c + right_i)
-        scanning_left = scanning_right = true
-        left_token, right_token = token_at(left, r), token_at(right, r)
-        loop do
-          scanning_left = false if left_token.nil?
-          scanning_right = false if right_token.nil?
+        c_with_dec, r_with_dec = (c + c_dec), (r + r_dec)
+        dec_token = token_at(c_with_dec, r_with_dec)
 
-          break unless scanning_left && scanning_right
+        c_with_inc, r_with_inc = (c + c_inc), (r + r_inc)
+        inc_token = token_at(c_with_inc, r_with_inc)
 
-          if scanning_left && should_stop.call(left_token)
-            result.concat(flip_so_far_left)
-            scanning_left = false
+        scanning_dec = scanning_inc = true
+        while scanning_dec || scanning_inc
+          if scanning_dec
+            scanning_dec = false if dec_token.nil?
+
+            if dec_token && dec_token.color == stop_color
+              result.concat(flip_so_far_dec)
+              scanning_dec = false
+            end
+
+            flip_so_far_dec.push([c_with_dec, r_with_dec])
+            c_with_dec, r_with_dec = (c_with_dec + c_dec), (r_with_dec + r_dec)
+            dec_token = token_at(c_with_dec, r_with_dec)
           end
 
-          if scanning_right && should_stop.call(right_token)
-            result.concat(flip_so_far_right)
-            scanning_right = false
+          if scanning_inc
+            scanning_inc = false if inc_token.nil?
+
+            if inc_token && inc_token.color == stop_color
+              result.concat(flip_so_far_inc)
+              scanning_inc = false
+            end
+
+            flip_so_far_inc.push([c_with_inc, r_with_inc])
+            c_with_inc, r_with_inc = (c_with_inc + c_inc), (r_with_inc + r_inc)
+            inc_token = token_at(c_with_inc, r_with_inc)
           end
-
-          flip_so_far_left.push([left, r])
-          flip_so_far_right.push([right, r])
-
-          left += left_i
-          right += right_i
-
-          left_token, right_token = token_at(left, r), token_at(right, r)
-        end
-
-        result
-      end
-
-      def scan_vertical(c, r, should_stop)
-        result, flip_so_far_up, flip_so_far_down = [], [], []
-        up_i, down_i = -1, 1
-
-        up, down = (r + up_i), (r + down_i)
-        scanning_up = scanning_down = true
-        up_token, down_token = token_at(c, up), token_at(c, down)
-        loop do
-          scanning_up = false if up_token.nil?
-          scanning_down = false if down_token.nil?
-
-          break unless scanning_up && scanning_down
-
-          if scanning_up && should_stop.call(up_token)
-            result.concat(flip_so_far_up)
-            scanning_up = false
-          end
-
-          if scanning_down && should_stop.call(down_token)
-            result.concat(flip_so_far_down)
-            scanning_down = false
-          end
-
-          flip_so_far_up.push([c, up])
-          flip_so_far_down.push([c, down])
-
-          up += up_i
-          down += down_i
-          up_token, down_token = token_at(c, up), token_at(c, down)
-        end
-
-        result
-      end
-
-      def scan_diagonal_negative_slope(c, r, should_stop)
-        result, flip_so_far_left_up, flip_so_far_right_down = [], [], []
-        left_i = up_i = -1
-        right_i = down_i = 1
-
-        lu_c, lu_r = (c + left_i), (r + up_i)
-        rd_c, rd_r = (c + right_i), (r + down_i)
-        scanning_lu = scanning_rd = true
-        lu_token, rd_token = token_at(lu_c, lu_r), token_at(rd_c, rd_r)
-        loop do
-          scanning_lu = false if lu_token.nil?
-          scanning_rd = false if rd_token.nil?
-
-          break unless scanning_lu && scanning_rd
-
-          if scanning_lu && should_stop.call(lu_token)
-            result.concat(flip_so_far_left_up)
-            scanning_lu = false
-          end
-
-          if scanning_rd && should_stop.call(rd_token)
-            result.concat(flip_so_far_right_down)
-            scanning_rd = false
-          end
-
-          flip_so_far_left_up.push([lu_c, lu_r])
-          flip_so_far_right_down.push([rd_c, rd_r])
-
-          lu_c += left_i
-          lu_r += up_i
-          rd_c += right_i
-          rd_r += down_i
-
-          lu_token, rd_token = token_at(lu_c, lu_r), token_at(rd_c, rd_r)
-        end
-
-        result
-      end
-
-      def scan_diagonal_positive_slope(c, r, should_stop)
-        result, flip_so_far_left_down, flip_so_far_right_up = [], [], []
-        left_i = up_i = -1
-        right_i = down_i = 1
-
-        ld_c, ld_r = (c + left_i), (r + down_i)
-        ru_c, ru_r = (c + right_i), (r + up_i)
-        scanning_ld, scanning_ru = true
-        ld_token, ru_token = token_at(ld_c, ld_r), token_at(ru_c, ru_r)
-        loop do
-          scanning_ld = false if ld_token.nil?
-          scanning_ru = false if ru_token.nil?
-
-          break unless scanning_ld && scanning_ru
-
-          if scanning_ld && should_stop.call(ld_token)
-            result.concat(flip_so_far_left_down)
-            scanning_ld = false
-          end
-
-          if scanning_ru && should_stop.call(ru_token)
-            result.concat(flip_so_far_right_up)
-            scanning_ru = false
-          end
-
-          flip_so_far_left_down.push([ld_c, ld_r])
-          flip_so_far_right_up.push([ru_c, ru_r])
-
-          ld_c += left_i
-          ld_r += down_i
-          ru_c += right_i
-          ru_r += up_i
-
-          ld_token, ru_token = token_at(ld_c, ld_r), token_at(ru_c, ru_r)
         end
 
         result
